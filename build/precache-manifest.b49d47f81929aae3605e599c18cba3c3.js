ea936ffe7482dcd258fb95d7e5cc150f1f065eb6
Vedanti00 <vedantisinkar959@gmail.com>
2023-02-14 16:21:20 +0530

    build

---
+++ build/precache-manifest.b49d47f81929aae3605e599c18cba3c3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b498f453443953d09e47f8f5cd295b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7592936c650e2395c32",
+    "url": "/static/js/2.e06bb6bb.chunk.js"
+  },
+  {
+    "revision": "0936574940502390b53b",
+    "url": "/static/js/main.bb93086c.chunk.js"
+  },
+  {
+    "revision": "9822f0b4eca1d99b7bcd",
+    "url": "/static/js/runtime-main.9ca938c0.js"
+  }
+]);
